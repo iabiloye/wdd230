151f80488b25aee8436054a2c4f20dc2ee5bcd9b
iabiloye <iabiloye@gmail.com>
2024-04-11 22:27:51 +0100

    update

---
+++ finalproject/scripts/closemasage.js
@@ -0,0 +1,8 @@
+
+function closeMessage() {
+    var closeableMessage = document.querySelector('.closeable-message');
+    if (closeableMessage) {
+        closeableMessage.style.display = 'none';
+    }
+}
+
